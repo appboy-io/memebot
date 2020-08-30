7faa73b0f2839b2af5b2b53ac609435d35d5ff8f
Bryan Riley <brileydeveloper@gmail.com>
2020-08-30 00:17:09 -0400

    Lain chan beginning code

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const fetch = require("node-fetch");
 const Discord = require("discord.js");
 
+const lainBoards = require("./constants/lain-chan-boards");
+
 const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
@@ -13,15 +15,22 @@ if (!botToken) {
 const supportedMessageBoards = new Map([
   ["4chan", "https://a.4cdn.org/boards.json"],
   //["7chan"],
-  //["lainchan"],
+  ["lainchan"],
 ]);
 
 const catalogLinksMap = new Map([
   ["4chan", "https://a.4cdn.org/{chosen-board}/catalog.json"],
+  ["lainchan", "https://lainchan.org/{chosen-board}/catalog.json"],
 ]);
 
-const cdnMap = new Map([["4chan", "https://i.4cdn.org/"]]);
-const contentApiMap = new Map([["4chan", "https://a.4cdn.org/"]]);
+const cdnMap = new Map([
+  ["4chan", "https://i.4cdn.org/"],
+  ["lainchan", "https://lainchan.org/"],
+]);
+const contentApiMap = new Map([
+  ["4chan", "https://a.4cdn.org/"],
+  ["lainchan", "https://lainchan.org/"],
+]);
 
 client.on("ready", () => {
   console.log("Connected as " + client.user.tag);
@@ -65,14 +74,14 @@ client.on("message", async (receivedMessage) => {
         if (args.length > 1) {
           console.log("fetching threads");
           let selectedBoard = args[0];
-          let board = args[1];
+          let board = encodeURI(args[1]);
           let threadList = await fetchCatalog(selectedBoard, board);
 
           receivedMessage.channel.send(
             selectedBoard +
               " is the board you selected, getting threads from: " +
-              board +
-              "use the '👌' to get media from a thread \n" +
+              decodeURI(board) +
+              " use the '👌' to get media from a thread \n" +
               "Media only available for a minute. If it doesn't download, reinput command"
           );
 
@@ -91,7 +100,7 @@ client.on("message", async (receivedMessage) => {
         break;
       default:
         receivedMessage.channel.send(
-          "The booty verse accepts many things, but not that command"
+          "The memeverse accepts many things, but not that command"
         );
         break;
     }
@@ -119,7 +128,7 @@ client.on("message", async (receivedMessage) => {
       console.log("Chosen Thread: " + chosenThread[1]);
       console.log("Chosen Forum: " + chosenThread[2]);
 
-      let board = chosenThread[0].trim().toLowerCase();
+      let board = encodeURI(chosenThread[0].trim()); //.trim(); //.toLowerCase();
       let thread = chosenThread[1];
       let forum = chosenThread[2];
 
@@ -141,6 +150,11 @@ async function fetchBoards(messageBoard) {
         .map((board) => board.title + "=>" + board.board)
         .join(",\n");
       return newBoards;
+    case "lainchan":
+      let boards = await lainBoards.boards
+        .map((board) => board.name + "=>" + board.symbol)
+        .join(",\n");
+      return boards;
     default:
       return "No boards available for " + messageBoard;
   }
@@ -149,41 +163,49 @@ async function fetchBoards(messageBoard) {
 async function fetchCatalog(messageBoard, chosenBoard) {
   switch (messageBoard) {
     case "4chan":
-      let catalogUrl = catalogLinksMap
-        .get(messageBoard)
-        .replace("{chosen-board}", chosenBoard);
-      let result = await fetch(catalogUrl);
-      let data = await result.json();
-      let threadList = data
-        .filter((element) => element.page === 1 || element.page === 2)
-        .map((page) => page.threads)
-        .flat();
-      console.log("Thread list content: " + Object.keys(threadList[0]));
-      let threadTitles = threadList
-        .filter((thread) => thread.sub)
-        .filter((thread) => !thread.sticky)
-        .map(
-          (thread) =>
-            thread.sub.replace(",", " ") +
-            ": " +
-            "Replies [" +
-            thread.replies +
-            "]" +
-            ", " +
-            chosenBoard.toUpperCase() +
-            ":" +
-            thread.no +
-            ":" +
-            "4chan"
-        );
-      return threadTitles;
+    case "lainchan":
+      return await assembleCatalog(messageBoard, chosenBoard);
+    default:
+      break;
   }
 }
 
+async function assembleCatalog(messageBoard, chosenBoard) {
+  console.log("chosen board: " + chosenBoard);
+  let catalogUrl = catalogLinksMap
+    .get(messageBoard)
+    .replace("{chosen-board}", chosenBoard);
+  let result = await fetch(catalogUrl);
+  let data = await result.json();
+  let threadList = data
+    .filter((element) => element.page === 1 || element.page === 2)
+    .map((page) => page.threads)
+    .flat();
+  console.log("Thread list content: " + Object.keys(threadList[0]));
+  let threadTitles = threadList
+    .filter((thread) => thread.sub)
+    .filter((thread) => !thread.sticky)
+    .map(
+      (thread) =>
+        thread.sub.replace(",", " ") +
+        ": " +
+        "Replies [" +
+        thread.replies +
+        "]" +
+        ", " +
+        decodeURI(chosenBoard).toUpperCase() +
+        ":" +
+        thread.no +
+        ":" +
+        messageBoard
+    );
+  return threadTitles;
+}
+
 async function fetchContent(board, thread, forum) {
-  let cdn = cdnMap.get(forum);
-  let contentApiUrl = contentApiMap.get(forum);
-  let url = contentApiUrl + board + "/thread/" + thread + ".json";
+  let url = await assembleContentApiUrl(board, thread, forum);
+
+  console.log("URL for content: " + url);
 
   let contentResult = await fetch(url);
 
@@ -191,7 +213,38 @@ async function fetchContent(board, thread, forum) {
 
   let content = contentJson.posts
     .filter((post) => post.ext)
-    .map((post) => cdn + board + "/" + post.tim + post.ext);
-
+    .map((post) => assembleContentPath(post, forum, board));
   return content;
+}
+
+async function assembleContentApiUrl(board, thread, forum) {
+  let contentApiUrl = contentApiMap.get(forum);
+
+  console.log("Content API Url: " + contentApiUrl);
+  switch (forum) {
+    case "4chan":
+      let fourChanUrl = contentApiUrl + board + "/thread/" + thread + ".json";
+      return fourChanUrl;
+    case "lainchan":
+      let sampleFilter = lainBoards.boards.filter((b) => b.symbol === board);
+      console.log("Comparing symbols: " + sampleFilter.length);
+      console.log("Direct comparison: " + board === "Δ");
+      let lainChanUrl =
+        contentApiUrl + encodeURIComponent(board) + "/res/" + thread + ".json";
+      console.log("Lainchan URL: " + lainChanUrl);
+      return lainChanUrl;
+    default:
+      break;
+  }
+}
+
+function assembleContentPath(post, forum, board) {
+  let cdn = cdnMap.get(forum);
+
+  switch (forum) {
+    case "4chan":
+      return cdn + board + "/" + post.tim + post.ext;
+    case "lainchan":
+      return cdn + board + "/src/" + post.tim + post.ext;
+  }
 }
---
+++ constants/lain-chan-boards.js
@@ -0,0 +1,18 @@
+const boards = [
+  { name: "Programming", symbol: "λ", alt: "prog" },
+  { name: "Do It Yourself", symbol: "Δ", alt: "diy" },
+  { name: "Security", symbol: "sec", alt: "sec" },
+  { name: "Technology", symbol: "Ω", alt: "tech" },
+  { name: "Games and Interactive Media", symbol: "inter", alt: "games" },
+  { name: "Music and Audible Media", symbol: "music", alt: "music" },
+  { name: "Visual Media", symbol: "vis", alt: "vis" },
+  { name: "Humanity", symbol: "hum", alt: "hum" },
+  { name: "Drugs 3.0", symbol: "drug", alt: "drug" },
+  { name: "Consciousness and Dreams", symbol: "zzz", alt: "zzz" },
+  { name: "layer", symbol: "layer", alt: "layer" },
+  { name: "questions", symbol: "q", alt: "q" },
+  { name: "Random", symbol: "r", alt: "r" },
+  { name: "lain", symbol: "lain", alt: "lain" },
+];
+
+module.exports = { boards };
